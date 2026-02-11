55e295e86797a76ec83bd611f17b023e398f5fed
Max Miller <maxrmiller55@gmail.com>
2026-02-10 21:34:02 -0700

    fixed franchise tests

--- tests/franchise.spec.ts
+++ tests/franchise.spec.ts
@@ -638,6 +638,7 @@ test('franchisee page shows revenue increase after ordering a pizza', async ({ p
   await expect(page.getByRole('button', { name: 'Close' })).toBeVisible();
   await page.getByRole('button', { name: 'Close' }).click();
   
+  await expect(page.getByRole('heading', { name: 'JWT Pizza - valid' })).not.toBeVisible();
   await expect(page.getByRole('button', { name: 'Close' })).not.toBeVisible();
 
   //check that TEST store revenue increased
